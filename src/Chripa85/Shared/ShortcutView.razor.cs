95140866b0937db97cea8267ad9f639693798421
Christoffer Pagaard <christoffer@pagaard.net>
2022-08-19 00:16:14 +0200

    not nice, but it works

--- src/Chripa85/Shared/ShortcutView.razor.cs
+++ src/Chripa85/Shared/ShortcutView.razor.cs
@@ -5,11 +5,33 @@ namespace Chripa85.Shared;
 
 public partial class ShortcutView : ComponentBase
 {
+    private string _selectedText = "";
+    bool _isSelected = false;
+    private IEnumerable<Shortcut> _selectedShortcuts = Enumerable.Empty<Shortcut>();
+
+    private IEnumerable<string> _categoryNames = Enumerable.Empty<string>();
+
+
     [Parameter]
-    public ShortcutMap ShortcutMap { get;set; }
+    public ShortcutMap ShortcutMap { get; set; }
 
     protected override void OnParametersSet()
     {
+        _categoryNames = ShortcutMap.GetCategoryNames();
+    }
+
+    private void UpdateSelection(string val)
+    {
+        if (val == string.Empty)
+        {
+            _selectedShortcuts = Enumerable.Empty<Shortcut>();
+            _isSelected = false;
+        }else
+        {
+            _selectedShortcuts = ShortcutMap.GetCategory(val);
+            _isSelected = true;
+        }
         
+        StateHasChanged();
     }
 }
